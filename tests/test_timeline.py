2bca4885ff0da369fa32baa99b6c08e9b55083c9
Eric Bragas <ericbragas412@gmail.com>
2020-12-31 12:20:48 -0800

    init story class and tests

--- tests/test_timeline.py
+++ tests/test_timeline.py
@@ -1,11 +1,21 @@
 import pendulum
-from timeline.timeline import Timeline
+from timeline.timeline import Timeline, Story
 
-
+## Timeline Class ## 
 def test_timeline_init():
-    """Test initialization options"""
+    """Test initialization options of Timeline"""
     timeline1 = Timeline()
     assert timeline1.start_date == pendulum.today()
     
     timeline2 = Timeline(start_date='1993-02-18')
     assert timeline2.start_date == pendulum.datetime(1993, 2, 18, tz='local')
+
+
+## Story Class ##
+def test_story_init():
+    """Test initilization options of Story"""
+    timeline = Timeline()
+    story1 = Story(timeline=timeline)
+    assert story1.start_date == pendulum.today()
+    assert story1.title == pendulum.today().to_datetime_string()
+    assert str(story1).endswith("num_entries: 0>")
--- timeline/timeline.py
+++ timeline/timeline.py
@@ -1,8 +1,41 @@
 import pendulum
+from uuid import uuid4 as uuid
+
+
+def today_str():
+    return pendulum.today().to_datetime_string()
+
 
 class Timeline:
     """The container for your stories"""
     
-    def __init__(self, start_date: str = pendulum.today().to_datetime_string()):
-        self.stories = []
+    def __init__(self, start_date: str = today_str()):
+        self.stories = set()
         self.start_date = pendulum.parse(start_date, tz="local")
+
+
+class Story:
+    """A group of 1 or more Entries that create a narrative or should otherwise
+    be grouped"""
+
+    def __init__(
+        self, 
+        timeline: Timeline,
+        start_date: str = today_str(),
+        title: str = today_str(),
+    ):
+        self.timeline = timeline
+        self.timeline.stories.add(self)
+        self.entries = []
+        self.uuid = uuid()
+        self.start_date = pendulum.parse(start_date, tz="local")
+        self.title = title
+
+    def __repr__(self):
+        return f'<uuid: {self.uuid} title: "{self.title}" num_entries: {len(self.entries)}>'
+
+
+# NOTE: dev only
+if __name__ == "__main__":
+    t = Timeline()
+    s = Story(t)
