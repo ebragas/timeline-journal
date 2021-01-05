0c6d964f362df4781cae4c73646dd08f0b3c63f9
Eric Bragas <ericbragas412@gmail.com>
2021-01-05 13:18:05 -0800

    refining tests

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,4 +1,8 @@
 from timeline.helpers import today_str, parse_datetime_local_tz
 
+# NOTE: choosing not to assert because I don't need to test that the library works
 def test_today_str():
-    pass
+    today_str()
+
+def test_parse_datetime_local_tz():
+    parse_datetime_local_tz("2021-01-01")
