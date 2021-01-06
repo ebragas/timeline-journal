cdd69fd99f0e179ec777b8b8c05c46ba430c61bd
Eric Bragas <ericbragas412@gmail.com>
2021-01-06 15:00:38 -0800

    remove story and update tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -1,48 +1,48 @@
-import sys
-import os
 import pytest
 
-from timeline.core import Timeline, Story, Entry
+from timeline import default_timeline
+from timeline.core import Timeline, Entry
 import pendulum
 
 
-
+## Fixtures
 @pytest.fixture
 def empty_timeline_fixture():
-    return Timeline()
+    t = Timeline()
+    yield t
+    del t
 
-## Timeline Class ##
-def test_timeline_init():
+@pytest.fixture
+def timeline_w_blank_entry_fixture(empty_timeline_fixture):
+    t = empty_timeline_fixture
+    _ = t.add_entry()
+    return t
+
+
+## Entry Class Tests
+def test_default_entry(timeline_w_blank_entry_fixture):
+    t = timeline_w_blank_entry_fixture
+    assert len(t.entries) == 1
+    blank_entry = t.entries[0]
+    assert isinstance(blank_entry.start_dt, pendulum.DateTime)
+    assert isinstance(blank_entry.end_dt, pendulum.DateTime)
+    assert blank_entry.start_dt.to_day_datetime_string() == blank_entry.title
+
+
+## Timeline Class Tests
+# TODO:
+def test_default_timeline():
+    assert isinstance(default_timeline, Timeline)
+
+def test_timeline_add_story(empty_timeline_fixture):
     """Test initialization options of Timeline"""
-    timeline1 = Timeline()
-    assert timeline1.start_date == pendulum.today()
-
-    timeline2 = Timeline(start_date="1993-02-18")
-    assert timeline2.start_date == pendulum.datetime(1993, 2, 18, tz="local")
-
-
-## Story Class ##
-def test_story_init(empty_timeline_fixture):
-    """Test initilization options of Story"""
-    # import pdb; pdb.set_trace()
-    timeline = empty_timeline_fixture
-    story1, _ = timeline.add_story()
-    assert story1.start_date == pendulum.today()
-    assert story1.title == pendulum.today().to_datetime_string()
-    assert str(story1).endswith("num_entries: 1>")
-    assert len(timeline.stories) == 1
-
-    story2, _ = timeline.add_story(start_date="1994-02-18", title="My first birthday")
-    assert story2.start_date == pendulum.datetime(1994, 2, 18, tz="local")
-    assert story2.title == "My first birthday"
-    assert len(timeline.stories) == 2
-
-
-## Entry Class ##
-def test_entry_init(empty_timeline_fixture):
-    """Test initilization options of Entry"""
-    timeline = empty_timeline_fixture
-    story, default_entry = timeline.add_story(start_date="1993-02-18", title="The beginning")
-    assert len(story.entries) == 1
-    story.add_entry(date="2021-01-01", body="The first day of the new year.")
-    assert len(story.entries) == 2
+    t = empty_timeline_fixture
+    new_entry = t.add_entry(
+        start_dt="1993-02-18",
+        # default end_dt same as start_dt
+        title="Day 1",
+        body="The day I was born."
+    )
+    assert isinstance(new_entry, Entry)
+    assert isinstance(new_entry.created_dt, pendulum.DateTime)
+    assert new_entry.created_dt == new_entry.modified_dt
